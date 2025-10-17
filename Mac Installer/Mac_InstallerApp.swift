56bb7d575d7a7dddfadca11df5623db54d9a75d1
doroved <dorroved@gmail.com>
2025-10-17 12:59:59 +0300

    Initial Commit

---
+++ Mac Installer/Mac_InstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_InstallerApp.swift
+//  Mac Installer
+//
+//  Created by doroved on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mac_InstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
